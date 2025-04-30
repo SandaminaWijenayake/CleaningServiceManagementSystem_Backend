8c77268e45c2551e947370c344381fd1ef500222
SandaminaWijenayake <sandaminawijenayake0717@gmail.com>
2025-05-01 05:25:39 +0530

    cors

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
+
+server.use(
+  cors({
+    origin: "https://cleaningservicemanagementsystem.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 server.use(middlewares);
 server.use(router);
