4c0f7fd34f254035ffc097b200a0241ecb8ac9f7
SandaminaWijenayake <sandaminawijenayake0717@gmail.com>
2025-05-01 06:56:25 +0530

    ewf

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const cors = require("cors");
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const cors = require("cors");
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-server.use(
-  cors({
-    origin: "https://cleaningservicemanagementsystem.netlify.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// server.use(
+//   cors({
+//     origin: "https://cleaningservicemanagementsystem.netlify.app",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
